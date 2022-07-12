69c713498aae757a0522442b19106708cc96ad5b
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-11 21:56:49 -0700

    added comments

--- src/PaintersPartitionProblem.java
+++ src/PaintersPartitionProblem.java
@@ -1,6 +1,8 @@
 public class PaintersPartitionProblem {
 
-
+        //a method to check if the mid satisfy the situation, or nay other number in the array
+    // of containing low to high;
+    // also gives me the count
     public static boolean check(int arr[], int n , int k, long mid){
         long work = 0;
         long count = 1;
@@ -29,18 +31,26 @@ public class PaintersPartitionProblem {
         //high is the time when only one painter do all the paints
         long high = 0;
 
-
+        // get the values of high and low
         for(int i= 0; i<n; i++){
             low = Math.max(low, arr[i]);
             high += arr[i];
         }
+
+
+        // after getting high and low,
+        // store them in an array low to high  and check them accordingly
         long ans = -1;
         while(low<= high){
+            // mid
             long mid = (low + high)/2;
+           // check if mid satisfies the condition and if k<=n;
             if((check(arr, n, k, mid))){
+                //store mid in ans and then repeat the iteration with other half of array
                 ans = mid;
                 high = mid-1;
             }else
+            // else amount of work is more than mid for painter then check the other half of the array
             {
                 low = mid+1;
             }
