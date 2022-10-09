cfd2a772cddc6b496922da337e14f4e1f6575e0e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-10-08 19:17:22 -0700

    had some comments that did not make any sense for the code reading, updated them and now going to push them

--- src/PaintersPartitionProblem.java
+++ src/PaintersPartitionProblem.java
@@ -1,12 +1,13 @@
 public class PaintersPartitionProblem {
 
-        //a method to check if the mid satisfy the situation, or nay other number in the array
-    // of containing low to high;
-    // also gives me the count
+
     public static boolean check(int arr[], int n , int k, long mid){
+        // count =1 as I am assuming 1 painter working on it first
         long work = 0;
         long count = 1;
 
+        //if work and the arr[i] < mid, means if the work to do all is smaller than mid , then we can add more work for one painter
+        //else we need anther painter, and that painter will start working from the next board so work will be arr[i]
         for(int i=0; i<n; i++){
             if(work + arr[i]< mid){
                 work += arr[i];
@@ -15,6 +16,10 @@ public class PaintersPartitionProblem {
                 work = arr[i];
             }
         }
+
+        // after all the iterations if i get count <=k, which satisfies my parameters, i will return true
+        //else false
+        //and false will eliminate all the small time possibilities and update the low with mid+1 for next iteration
         if(count<= k){
             return true;
         }else{
@@ -26,39 +31,32 @@ public class PaintersPartitionProblem {
 
     public static long paintersPartitionProblem(int []arr, int k, int n){
 
-        // low will be the time when n= k and they do all the paints together.
+        //low is storing the atleast time, which should be when k=n and all of them work together and which is max element of the array
+        // high is if there is only one painter k=1, so sum of the whole array.
         long low = arr[0];
-        //high is the time when only one painter do all the paints
         long high = 0;
 
-        // get the values of high and low
         for(int i= 0; i<n; i++){
             low = Math.max(low, arr[i]);
             high += arr[i];
         }
 
-
-        // after getting high and low,
-        // store them in an array low to high  and check them accordingly
+        //now the answer lies in between low and high so run a loop to get it, we will perform binary search
+        // get a mid and check if it satisfies the situation in check method and eliminate all the greater possibilities
+        // else eliminate all the small times and start with mid+1 in else statement.
+        //update the answer with mid all the times and return it.
         long ans = -1;
         while(low<= high){
-            // mid
             long mid = (low + high)/2;
-           // check if mid satisfies the condition and if k<=n;
             if((check(arr, n, k, mid))){
-                //store mid in ans and then repeat the iteration with other half of array
                 ans = mid;
                 high = mid-1;
-            }else
-            // else amount of work is more than mid for painter then check the other half of the array
+            }
+            else
             {
                 low = mid+1;
             }
         }return ans;
-
-
-
-
     }
 
 
